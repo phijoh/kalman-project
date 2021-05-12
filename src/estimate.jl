d976184a2d5dccc5e8aee606a4ae5d0e674b5aa5
NoFishLikeIan <andreatitton96@gmail.com>
2021-05-12 10:51:44 +0200

    🧹 nicer loop handling

--- src/estimate.jl
+++ src/estimate.jl
@@ -1,4 +1,4 @@
-@model movement(x, u, Δt) = begin
+@model function movement(x, u, Δt) 
 
     T, M = size(x)
 
@@ -7,26 +7,23 @@
     Dᵥ ~ InverseGamma(2, 0.5)
     σₚ² ~ InverseGamma(2, 0.5)
 
-    νₓ = Dₓ 
-    νᵤ = inv(inv(σₚ²) + inv(Dᵥ))
-    γ = inv(1 + Dᵥ / σₚ²)
-
-    x[1, :] ~ MvNormal(x[1, :], νₓ * Δt)
-    u[1, :] ~ MvNormal(u[1, :], νᵤ * Δt)
-
-    for t = 2:T
+    for t = 1:T
         
         νₓ = Dₓ 
         νᵤ = inv(inv(σₚ²) + inv(Dᵥ))
         γ = inv(1 + Dᵥ / σₚ²)
 
-        xₚ = x[t - 1, :]
-        uₚ = u[t - 1, :]
+        xₚ = t > 1 ? x[t - 1, :] : x[1, :]
+        uₚ = t > 1 ? u[t - 1, :] : u[1, :]
 
         x[t, :] ~ MvNormal(xₚ + Δt * uₚ, νₓ * Δt)
-        u[t, :] ~ MvNormal(γ * uₚ , νᵤ * Δt)
+        u[t, :] ~ MvNormal(γ * uₚ, νᵤ * Δt)
 
     end
     
 end
 
+@model function restrmovement(x, u, Δt)
+
+
+end
