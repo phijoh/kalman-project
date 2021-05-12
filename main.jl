7fa70d52bf01d77a2a5b80b7678e1487326f2e6c
NoFishLikeIan <andreatitton96@gmail.com>
2021-05-12 10:04:02 +0200

    🧹 estimation procedure

--- main.jl
+++ main.jl
@@ -1,18 +1,38 @@
+using LinearAlgebra
 using Random, Distributions
-using Turing
-using StatsPlots
+using Turing, ParticleFilters
+
+using Plots, StatsPlots
 
 Random.seed!(1123)
 
-include("src/dgp.jl")
+include("src/utils.jl")
 include("src/estimate.jl")
+include("src/dgp.jl")
 
-σₓ, σᵧ = 8., 1.
-vel₀ = zeros(2)
+Dₓ = 0.2
+Dᵥ = 0.25
+σₚ² = 0.25
 
-vel = dgp(σₓ, σᵧ)
-model = movement(nothing, vel, vel₀, 0.05)
-chain = sample(model, HMC(0.05, 10), 1000, progress=true)
+Δt = 0.05
+x₀ = [100., 0.]
+u₀ = [0., 0.]
 
-p_summary = chain[:h]
-plot(p_summary, seriestype=:histogram)
+x, u = dgp(x₀, u₀, Dₓ, Dᵥ, σₚ², Δt; T=4_000)
+
+model = movement(x, u, Δt)
+chain = sample(model, NUTS(0.65), 3_000)
+
+Dᵥest = mean(chain[:Dᵥ])
+Dₓest = mean(chain[:Dₓ])
+σₚ²est = mean(chain[:σₚ²])
+
+σᵥ = inv(inv(σₚ²) + inv(Dᵥ))
+γ = inv(1 + Dᵥ / σₚ²)
+
+σᵥest = inv(inv(σₚ²est) + inv(Dᵥest))
+γest = inv(1 + Dᵥest / σₚ²est)
+
+println("σᵥ = $σᵥ, σᵥest = $σᵥest")
+println("γ = $γ, γest = $γest")
+
