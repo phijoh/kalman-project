9cdfbd1e4caa8bc0284186cbbd46fc75d40bcd4c
NoFishLikeIan <andreatitton96@gmail.com>
2021-05-12 10:03:13 +0200

    🪐 khoei et al dgp

--- src/dgp.jl
+++ src/dgp.jl
@@ -1,17 +1,22 @@
 """
-Generates data with T time points and two variances
+Generates data with T time points
 """
-function dgp(σₓ, σᵧ; T=200)
+function dgp(x₀, u₀, Dₓ, Dᵥ, σₚ², Δt; T=200)
     
-    Σ = [σₓ 0; 0 σᵧ]
+    σᵥ = inv(σₚ²) + inv(Dᵥ)
+    γ = inv(1 + Dᵥ / σₚ²)
 
-    vel = zeros(T, 2)
+    νₓ = MvNormal(zeros(2), Dₓ * Δt)
+    νᵤ = MvNormal(zeros(2), inv(σᵥ) * Δt)
+
+    x = zeros(T, 2); x[1, :] = x₀
+    u = zeros(T, 2); u[1, :] = u₀
 
     for t in 2:T
-        veldist = MvNormal(vel[t - 1, :], Σ)
-        vel[t, :] = rand(veldist)
+        x[t, :] = x[t - 1, :] + u[t - 1, :] + rand(νₓ)
+        u[t, :] = γ * u[t - 1, :] + rand(νᵤ)
     end
 
-    return vel
-
+    return x, u
 end
+
