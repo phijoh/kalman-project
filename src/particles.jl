541663638efef210870a6dfa272d4d865c88ff94
NoFishLikeIan <andreatitton96@gmail.com>
2021-09-22 18:13:26 +0200

    🐛 particle size from global should have been local

--- src/particles.jl
+++ src/particles.jl
@@ -21,19 +21,21 @@ function selectrandomparticles(framesize, nparticles::Int64)
 
 end
 
-
+"""
+Get the luminance around (rfsize) the particles
+"""
 function getparticlevalues(frame::Matrix{Float64}, particles::Matrix{Int64}; rfsize=2)
     
     #TODO: smooth whole frame?
 
-    width, height = size(frame)
+    width = size(frame, 1) # FIXME: What happens with height?
 
     particlevalues = zeros(size(particles,1))
 
-    for (i,particle) in eachrow(particles) |> enumerate
-
-        x₀,y₀ = max.(particle .- (rfsize-1), 1)
-        x₁,y₁ = min.(particle .+ (rfsize-1), width)
+    for (i, particle) in eachrow(particles) |> enumerate
+        (x, y, u, v) = particle
+        x₀,y₀ = max.([x, y] .- (rfsize-1), 1)
+        x₁,y₁ = min.([x, y] .+ (rfsize-1), width)
 
         particlevalues[i] = mean(frame[x₀:x₁,y₀:y₁])
 
@@ -43,15 +45,19 @@ function getparticlevalues(frame::Matrix{Float64}, particles::Matrix{Int64}; rfs
 
 end
 
-
+"""
+Drop particles with low weight and substitute with high weight particles
+"""
 function duplicateparticles!(particles::Matrix{Int64}, weights::Vector{Float64})
+
+    N = size(particles, 1)
 
     threshold = mean(weights)
     discardedparticleidx = weights .< threshold
 
     bestparticles = particles[.!discardedparticleidx,:]
 
-    for i in (1:nparticles)[discardedparticleidx]
+    for i in (1:N)[discardedparticleidx]
         
         duplicate = sample(1:size(bestparticles,1))
         particles[i,:] = bestparticles[duplicate,:]
