b0d20c0fae3c800a00f8e75dad59d862e33ab257
NoFishLikeIan <andreatitton96@gmail.com>
2021-12-03 18:54:24 +0100

    🦋 double sampling: particle - sampling

--- src/estimate.jl
+++ src/estimate.jl
@@ -1,28 +1,69 @@
-@model function movement(x, u, Δt)
+@model function movement(p′, p, Σprior)
 
-    T = size(x, 1)
+    N = size(p, 1)
 
-    # Parameters to estimate
-    Dₓ ~ InverseGamma(2, 1)
-    Dᵥ ~ InverseGamma(2, 1)
-    σₚ² ~ InverseGamma(2, 1)
+    Σ ~ Σprior
 
-    # Initial position
-    x[1, :] ~ MvNormal([0., 0.], 1.)
-    u[1, :] ~ MvNormal([0., 0.], 1.)
-
-    for t = 2:T
-
-        νₓ = Dₓ 
-        νᵤ = inv(inv(σₚ²) + inv(Dᵥ))
-        γ = inv(1 + Dᵥ / σₚ²)
-
-        uₑ = u[t - 1, :] * γ
-        xₑ = x[t - 1, :] + Δt * uₑ
-
-        x[t, :] ~ MvNormal(xₑ, νₓ * Δt)
-        u[t, :] ~ MvNormal(uₑ, νᵤ * Δt)
-
+    for i in 1:N
+        p′[i, :] ~ MvNormal(p[i, :], Σ)
     end
     
+end
+
+function stepwiseparticle(T, N, frames; dimensions = 4, verbose = false, L = 500)
+
+    width, height = size(first(frames))
+
+    chains = []
+
+    # Initialize weights and particles
+    particles₀, weights₀ = selectrandomparticles((width, height), N)
+    
+    weightsovertime = zeros(T, N)
+    weightsovertime[1, :] = weights₀
+    
+    particlesovertime = zeros(Int64, T, N, dimensions)
+    particlesovertime[1, :, :] = particles₀
+    
+    # Initialize prior
+    Σ = MvLogNormal(MvNormal(zeros(dimensions), diagm(ones(dimensions))))
+
+    for t in 2:T
+        verbose && print("Iteration t = $t / $T")
+
+        # One particle step with a random sample from the prior
+        Σ̂ = rand(Σ) |> diagm
+
+        particles, weights, losers = step(
+            particlesovertime[t-1, :, :], weightsovertime[t-1, :], 
+            frames, t, Σ̂, σ²ᵢ
+        )
+
+        # Estimate the posterior Σ using the winning particles
+        notlosers = .~losers
+        model = movement(particles[notlosers, :], prevparticles[notlosers, :], Σ)
+
+        chain = sample(model, NUTS(0.65), L)
+
+        # Make posterior new prior
+        μₛ = zeros(dimensions)
+        σ²ₛ = zeros(dimensions)
+
+        for i in 1:dimensions
+            μ = mean(chain.value[:, i])
+            σ² = var(chain.value[:, i])
+
+            μₛ[i], σ²ₛ[i] = logNtoN(μ, σ²)
+        end
+
+        Σ = MvLogNormal(MvNormal(μₛ , diagm(sqrt.(σ²ₛ))))
+
+        # Save particles and chain
+        particlesovertime[t, :, :] = particles
+        weightsovertime[t, :] = weights
+        push!(chains, chain)
+
+    end
+
+    return particlesovertime, weightsovertime, chains
 end
