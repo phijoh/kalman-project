38e85364a7db7078dbed9067e41e56bca49904dd
NoFishLikeIan <andreatitton96@gmail.com>
2022-04-21 16:16:23 +0200

    🐛 fixed bugs in particle steps

--- src/particles.jl
+++ src/particles.jl
@@ -7,13 +7,13 @@ function selectrandomparticles(framesize, nparticles::Int64)
     x = sample(1:width, nparticles)
     y = sample(1:height, nparticles)
 
-    u = sample(-(width ÷ 100):(width ÷ 100), nparticles)
-    v = sample(-(height ÷ 100):(height ÷ 100), nparticles)
+    u = sample(-(width ÷ 100):(width÷100), nparticles)
+    v = sample(-(height ÷ 100):(height÷100), nparticles)
 
     particles[:, 1] = x
     particles[:, 2] = y
-    particles[:, 3] .= 0
-    particles[:, 4] .= 0
+    particles[:, 3] .= u
+    particles[:, 4] .= v
 
     weights = ones(nparticles) ./ nparticles
 
@@ -25,7 +25,7 @@ end
 Get the luminance around (rfsize) the particles
 """
 function getparticlevalues(frame::Frame, particles::Matrix{Int64}; rfsize=1)
-    
+
     # TODO: smooth whole frame?
 
     width = size(frame, 1) # FIXME: What happens with height?
@@ -35,8 +35,8 @@ function getparticlevalues(frame::Frame, particles::Matrix{Int64}; rfsize=1)
 
     for (i, particle) in eachrow(particles) |> enumerate
         (x, y, u, v) = particle
-        x₀,y₀ = max.([x, y] .- (rfsize-1), 1)
-        x₁,y₁ = min.([x, y] .+ (rfsize-1), width)
+        x₀, y₀ = max.([x, y] .- (rfsize - 1), 1)
+        x₁, y₁ = min.([x, y] .+ (rfsize - 1), width)
 
         particlevalues[i] = mean(frame[x₀:x₁, y₀:y₁])
 
@@ -57,12 +57,12 @@ function moveparticles(particles::Matrix{Int64}, rest...)::Matrix{Int64}
     return newparticles
 end
 
-function moveparticles!(particles::Matrix{Int64}, Σ::Matrix{Float64}, framesize::Tuple{Int64, Int64})
+function moveparticles!(particles::Matrix{Int64}, Σ::Matrix{Float64}, framesize::Tuple{Int64,Int64})
 
     width, height = framesize
     N = size(particles, 1)
 
-    ν = all(Σ .≈ 0) ? zeros(4, N) : rand(MvNormal(zeros(4), Σ), N)
+    ν = all(Σ .≈ 0) ? zeros(4, N) : rand(MvNormal(zeros(4), sqrt.(Σ)), N)
 
     x = particles[:, 1:2]
     V = particles[:, 3:4]
@@ -72,44 +72,39 @@ function moveparticles!(particles::Matrix{Int64}, Σ::Matrix{Float64}, framesize
 
     particles[:, 1] = torus.(x′[:, 1], width)
     particles[:, 2] = torus.(x′[:, 2], height)
-    
+
     particles[:, 3:4] = @. round(Int64, V′)
 
 end
 
 Σ₀ = zeros(4, 4)
 
-function likelihood(fr::Frame, fr′::Frame, particles, nextparticles, σ²ᵢ)
+function likelihood(fr::Frame, fr′::Frame, particles, nextparticles, σ²ᵢ; rfsize)
     # Find likelihood of luminance
-    I = getparticlevalues(fr, particles)
-    I′ = getparticlevalues(fr′, nextparticles)
+    I = getparticlevalues(fr, particles; rfsize=rfsize)
+    I′ = getparticlevalues(fr′, nextparticles; rfsize=rfsize)
     return pdf.(Normal(0, σ²ᵢ), @. (I - I′))
 end
 
 """
-Update particles and weights given the frames, a current time t, a covariance matrix of motion Σ, and an observation noise.
+Update particles and weights given the current frame and the next frame, a covariance matrix of motion Σ, and the luminance noise.
 """
-function step(particles, w, frames::Vector{Frame}, t, Σ, σ²ᵢ; trh = 0.5)
+function particlestep(particles, w, fr::Frame, fr′::Frame; Σ, σ²ᵢ, trh=0.5, rfsize=1)
     N = size(particles, 1)
-
-    fr = frames[t]
-    fr′ = frames[t + 1]
 
     # Compute realized particles
     particles′ = moveparticles(particles, Σ, size(fr))
-    L = likelihood(fr, fr′, particles, particles′, σ²ᵢ)
+    L = likelihood(fr, fr′, particles, particles′, σ²ᵢ, rfsize=rfsize)
 
     wᴬ = normalize(w .* L)
     wᴱ = mean(wᴬ) * trh
-    
+
     # Replace worst performing particles with theoretical particles
     losers = wᴬ .< wᴱ
     winners = sample((1:N)[.!losers], sum(losers))
 
     particles′[losers, :] .= particles′[winners, :]
     wᴬ[losers] = wᴬ[winners]
-    
-    # FIXME: Find nicer way to return losers
-    return particles′, normalize(wᴬ), losers
 
+    return particles′, normalize(wᴬ)
 end
