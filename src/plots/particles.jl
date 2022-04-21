1cafe94ad31a0a1c6f920708248cc83becc79c29
NoFishLikeIan <andreatitton96@gmail.com>
2022-04-21 16:16:38 +0200

    🎣 position estimates in utils

--- src/plots/particles.jl
+++ src/plots/particles.jl
@@ -1,4 +1,4 @@
-function plotparticledensity(particles::Matrix{Int64}, weights::Vector{Float64}, fr::Frame; rfsize = 2)
+function plotparticledensity(particles::Matrix{Int64}, weights::Vector{Float64}, fr::Frame; rfsize=2)
 
     width = size(fr, 1)
     N = size(particles, 1)
@@ -8,16 +8,16 @@ function plotparticledensity(particles::Matrix{Int64}, weights::Vector{Float64},
     for i in 1:N
         x, y = particles[i, 1:2]
 
-        x₀,y₀ = max.([x, y] .- (rfsize-1), 1)
-        x₁,y₁ = min.([x, y] .+ (rfsize-1), width)
+        x₀, y₀ = max.([x, y] .- (rfsize - 1), 1)
+        x₁, y₁ = min.([x, y] .+ (rfsize - 1), width)
 
         particle_density[x₀:x₁, y₀:y₁] .+= weights[i]
     end
 
     figure = contourf(
-        particle_density; 
-        size = (400, 400), legend = nothing, aspect_ratio = 1,
-        xlabel = L"x", ylabel = L"y", c = :Blues
+        particle_density;
+        size=(400, 400), legend=nothing, aspect_ratio=1,
+        xlabel=L"x", ylabel=L"y", c=:Blues
     )
 
     return figure
@@ -26,35 +26,40 @@ end
 
 function scatterparticles(particles, fr)
     S = size(fr, 1)
-    fig = heatmap(fr; aspect_ratio = 1, xlims = (0, S), ylims = (0, S), color = :greys)
+    fig = heatmap(fr; aspect_ratio=1, xlims=(0, S), ylims=(0, S), color=:greys)
 
-    scatter!(fig, particles[:, 2], particles[:, 1], label = nothing, color = :white)
+    scatter!(fig, particles[:, 2], particles[:, 1], label=nothing, color=:white)
 
     return fig
 end
 
-function plotexpectedposition(particles, weights, fr)
+function plotexpectedposition(particles, weights, fr; kwargs...)
     S = size(fr, 1)
-    fig = heatmap(fr; aspect_ratio = 1, xlims = (0, S), ylims = (0, S), color = :greys)
+    x, y = getexpectedposition(particles, weights)
+    Σ = getvariance(particles, weights)
 
-    x = mean(particles[:, 1], StatsBase.weights(weights))
-    y = mean(particles[:, 2], StatsBase.weights(weights))
+    Σₓ = sqrt.(Σ[1:2, 1:2])
+    N = MvNormal([x, y], Σₓ)
+    pdfN(x, y) = pdf(N, [x, y])
 
-    scatter!(fig, [x], [y], label = nothing, color = :red)
+    xs = ys = 0:1:S
+
+    fig = heatmap(
+        xs, ys, fr';
+        c=:grays, aspect_ratio=1,
+        colorbar=false,
+        xlims=ylims = (1, S), kwargs...
+    )
+
+    contour!(
+        fig,
+        xs, ys, (a, b) -> pdfN(a, b) / pdfN(x, y);
+        clim=(0, 1), c=:Reds,
+        colorbar=false
+    )
 
     return fig
 end
-
-function getexpectedvalue(p, w)
-
-    x = mean(p[:, 1], StatsBase.weights(w))
-    y = mean(p[:, 2], StatsBase.weights(w))
-
-    θ = xytoangle([x y]) .* (180 / π)
-
-    return θ
-end
-
 
 function plotangle(results, duration; kwargs...)
     S, T, _, _ = size(results[:particles])
@@ -67,7 +72,7 @@ function plotangle(results, duration; kwargs...)
         particlesovertime = @view results[:particles][s, :, :, :]
         weightsovertime = @view results[:weights][s, :, :]
 
-        θ₀ = getexpectedvalue(
+        θ₀ = getexpectedangle(
             results[:particles][s, duration, :, :],
             results[:weights][s, duration, :]
         )
@@ -78,31 +83,29 @@ function plotangle(results, duration; kwargs...)
             w = StatsBase.weights(weightsovertime[t, :])
 
             θ[t, s] = θ₀ - θₜ'w
-            σ[t, s] = std(θₜ, w) 
+            σ[t, s] = std(θₜ, w)
         end
     end
 
     varfig = plot(
-        xlabel = L"t", ylabel = L"\theta"; 
-        legend = :bottomleft, kwargs...)
+        xlabel=L"t", ylabel=L"\theta";
+        legend=:bottomleft, kwargs...)
 
     plott = 1:T
-    
+
     for s in 1:S
         plot!(
             varfig, plott, θ[plott, s];
-            ribbon = σ[plott, s], fillalpha = 0.2,
-            label = "speed = $(results[:speeds][s])")
+            ribbon=σ[plott, s], fillalpha=0.2,
+            label="speed = $(results[:speeds][s])")
     end
 
-    vline!(varfig, [duration], linecolor = :black, linestyle = :dot,
-    label = "wedge duration")    
+    vline!(varfig, [duration], linecolor=:black, linestyle=:dot,
+        label="wedge duration")
     # vline!(varfig, [duration]; linestyle = :dash, c = :black, label = "Disappearance")
 
     return varfig
 end
-
-
 
 function plotprecision(results, duration; kwargs...)
 
@@ -120,26 +123,26 @@ function plotprecision(results, duration; kwargs...)
             θₜ = xytoangle.(eachrow(p)) .* (180 / π)
             w = StatsBase.weights(weightsovertime[t, :])
 
-            σ[t, s] = var(p[:,1], w) + var(p[:,2], w)
+            σ[t, s] = var(p[:, 1], w) + var(p[:, 2], w)
         end
     end
 
     varfig = plot(
-        xlabel = L"t", ylabel = L"(\sigma_{x}^2 + \sigma_{y}^2)^{-1}"; 
-        legend = :outerright, kwargs...)
+        xlabel=L"t", ylabel=L"(\sigma_{x}^2 + \sigma_{y}^2)^{-1}";
+        legend=:outerright, kwargs...)
 
     plott = 1:T
-    
+
     for s in 1:S
         plot!(
-            varfig, plott, 1 ./σ[plott, s];
-            label = "speed = $results[:specs][s][1], 
-            opacity =  $results[:specs][s][2],
-            dynamic = $results[:specs][s][3]")
+            varfig, plott, 1 ./ σ[plott, s];
+            label="speed = $results[:specs][s][1], 
+          opacity =  $results[:specs][s][2],
+          dynamic = $results[:specs][s][3]")
     end
 
-    vline!(varfig, [duration-1], linecolor = :black, linestyle = :dot,
-    label = "stimulus duration")    
+    vline!(varfig, [duration - 1], linecolor=:black, linestyle=:dot,
+        label="stimulus duration")
     # vline!(varfig, [duration]; linestyle = :dash, c = :black, label = "Disappearance")
 
     return varfig
--- src/utilities/particles.jl
+++ src/utilities/particles.jl
@@ -1,1 +1,28 @@
 xytoangle(coord) = atan(coord[2] / coord[1])
+
+function getexpectedposition(p::Matrix{Int64}, w::Vector{Float64})
+
+    x = mean(p[:, 1], StatsBase.weights(w))
+    y = mean(p[:, 2], StatsBase.weights(w))
+
+    return x, y
+end
+
+function getvariance(p::Matrix{Int64}, w::Vector{Float64})
+    dimensions = size(p, 2)
+    Σ = zeros(dimensions, dimensions)
+
+    for d ∈ 1:dimensions
+        Σ[d, d] = var(p[:, d], StatsBase.weights(w))
+    end
+
+    return Σ
+end
+
+function getexpectedangle(p::Matrix{Int64}, w::Vector{Float64})
+
+    x, y = getexpectedposition(p, w)
+    θ = xytoangle([x y]) .* (180 / π)
+
+    return θ
+end
